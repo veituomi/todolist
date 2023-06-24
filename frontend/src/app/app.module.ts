143a31a1e2e0498651690721b3a672e0052874c8
Veini Tuominen <veini.tuominen@outlook.com>
2023-06-24 08:17:22 +0300

    Add Angular Material imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
 	declarations: [
@@ -11,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
+		MaterialModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
---
+++ frontend/src/app/material.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import {
+	MAT_FORM_FIELD_DEFAULT_OPTIONS,
+	MatFormFieldModule,
+} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+	exports: [
+		MatButtonModule,
+		MatCardModule,
+		MatDialogModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+	],
+	imports: [
+		MatButtonModule,
+		MatCardModule,
+		MatDialogModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+	],
+	providers: [
+		{
+			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+			useValue: {
+				appearance: 'outline',
+			},
+		}
+	],
+})
+export class MaterialModule {
+
+}
